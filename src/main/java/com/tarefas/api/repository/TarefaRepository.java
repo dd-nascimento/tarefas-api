f743f5a9f95082437766f683004015938978b0ca
David Nascimento Melo <dnmnascimento@outlook.com>
2024-07-23 00:10:57 -0300

    Endpoint para retorno por prioridade.

--- src/main/java/com/tarefas/api/repository/TarefaRepository.java
+++ src/main/java/com/tarefas/api/repository/TarefaRepository.java
@@ -1,5 +1,7 @@
 package com.tarefas.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.tarefas.api.model.Tarefas;
 
 @Repository
 public interface TarefaRepository extends JpaRepository <Tarefas, Long> {
+
+    Optional<Tarefas> delete(Long id);
+
+    Optional<Tarefas> findById(String prioridade);
     
 }
