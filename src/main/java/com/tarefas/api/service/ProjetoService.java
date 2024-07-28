ca60421f8ed783e2a8c99cef1562e98ceb4ed7f5
David Nascimento Melo <dnmnascimento@outlook.com>
2024-07-28 20:35:08 -0300

    Remoção dos Imports não utilizados.

--- src/main/java/com/tarefas/api/service/ProjetoService.java
+++ src/main/java/com/tarefas/api/service/ProjetoService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tarefas.api.model.Projetos;
-import com.tarefas.api.model.Usuario;
 import com.tarefas.api.repository.ProjetoRepository;
 
 @Service
